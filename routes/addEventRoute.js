fd498ced32690443fc7a379ffd73060bd787039e
Juan Felipe Arango <juan.felipe.arango@correounivalle.edu.co>
2023-06-05 15:20:19 -0500

    Mejora de escritura

--- routes/addEventRoute.js
+++ routes/addEventRoute.js
@@ -3,7 +3,7 @@ const routerAgregarEvento = express.Router();
 const {agregarEvento, editarEvento} = require('../controllers/addEventController.js');
 
 const routerEditarEvento = express.Router();
-const verificarAutenticacion =require('../middleware/verificarAutenticacion.js');
+const verificarAutenticacion = require('../middleware/verificarAutenticacion.js');
 
 routerEditarEvento.put('/evento/editar/:codigo_evento', editarEvento);
 
