d7fa4efa186c02d7b5f83526321f269a041c7c1c
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2023-06-09 14:21:40 -0500

    Correccion en secret key

--- controllers/controladorModuloLogin.js
+++ controllers/controladorModuloLogin.js
@@ -1,80 +1,79 @@
-const {pool} = require('../api/db.js');
-const {verifyGoogleCredential} = require('../api/AuthGoogle');
+const { pool } = require('../api/db.js');
+const { verifyGoogleCredential } = require('../api/AuthGoogle');
 const jwt = require('jsonwebtoken');
 
 
 const verificarIDBd = async (idcliente) => {
 
-   
-  
-    try {
-      const respuesta = await pool.query(`SELECT * FROM persona WHERE (id ='${idcliente}')`);
-     
-     
-      return respuesta;
-     
-    } catch (error) {
-     
-      console.log(error);
-    }
-  };
+
+
+  try {
+    const respuesta = await pool.query(`SELECT * FROM persona WHERE (id ='${idcliente}')`);
+
+
+    return respuesta;
+
+  } catch (error) {
+
+    console.log(error);
+  }
+};
 
 //funcion para verificar el login GOOGLE
 const verifyGoogleLogin = async (req, res) => {
 
-    console.log("entra aqui");
-  
-    console.log("VERIFICACION DE LA CREDENCIAL");
-  
-    console.log(req.body.data);
+  console.log("entra aqui");
 
-    const result = await verifyGoogleCredential(req.body.data.credential);
-  
-    console.log(result);
-    console.log(result.id);
-    console.log(result.email);
-    console.log(typeof result.id);
+  console.log("VERIFICACION DE LA CREDENCIAL");
 
-  
-    const VerificarBD = await verificarIDBd(result.id);
-  
-   // console.log(VerificarBD);
-   console.log(VerificarBD.rows);
-    //const bandera = true;
-  
-    if (VerificarBD.rowCount == 0) {
-  
-      console.log(VerificarBD.rowCount + " asdfghgfdsdsfgdsdfg");
+  console.log(req.body.data);
+
+  const result = await verifyGoogleCredential(req.body.data.credential);
+
+  console.log(result);
+  console.log(result.id);
+  console.log(result.email);
+  console.log(typeof result.id);
 
 
-      //se setea que no esta registrado
+  const VerificarBD = await verificarIDBd(result.id);
+
+  // console.log(VerificarBD);
+  console.log(VerificarBD.rows);
+  //const bandera = true;
+
+  if (VerificarBD.rowCount == 0) {
+
+    console.log(VerificarBD.rowCount + " asdfghgfdsdsfgdsdfg");
+
+
+    //se setea que no esta registrado
 
 
 
-    
-      res.send({
-        token: null
-      });
-      
-      
-    } else {
-  
-      console.log("asfdghfdsdfghgfdsdfghfdsfghfdsfghjgfdsfghj");
 
-      const json = VerificarBD.rows[0].id;
-
-      const token = jwt.sign(json, "ruizgei");
-     
-      res.send({
-        token: token
-      });
-  
-    }
-  
-  };
+    res.send({
+      token: null
+    });
 
 
-  module.exports = 
-    verifyGoogleLogin
+  } else {
 
-   
+    console.log("asfdghfdsdfghgfdsdfghfdsfghfdsfghjgfdsfghj");
+
+    const json = VerificarBD.rows[0].id;
+
+    const token = jwt.sign(json, "ds1g3");
+
+    res.send({
+      token: token
+    });
+
+  }
+
+};
+
+
+module.exports =
+  verifyGoogleLogin
+
