b6573ada4f51561ac4b086f01f4689e17d72180e
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2023-06-10 15:44:10 -0500

    Consultar persona porteado a modelo

--- controllers/addEventController.js
+++ controllers/addEventController.js
@@ -3,44 +3,45 @@ const evento = require('../models/evento.js');
 
 //Codigo que a traves de una consulta SQL manda toda la informacion que el front proporciona y crea un nuevo evento en la base de datos
 const agregarEvento = async (req, res) => {
-    try {
-        const { codigo_evento, descripcion, fecha, hora_inicio, hora_final, lugar, nombre } = req.body;
+  try {
+    const { codigo_evento, descripcion, fecha, hora_inicio, hora_final, lugar, nombre } = req.body;
 
-        await pool.query('INSERT INTO public.evento (codigo_evento, descripcion, fecha, hora_inicio, hora_final, lugar, nombre) VALUES($1, $2, $3, $4, $5, $6, $7);', [codigo_evento, descripcion, fecha, hora_inicio, hora_final, lugar, nombre]);
+    await pool.query('INSERT INTO public.evento (codigo_evento, descripcion, fecha, hora_inicio, hora_final, lugar, nombre) VALUES($1, $2, $3, $4, $5, $6, $7);', [codigo_evento, descripcion, fecha, hora_inicio, hora_final, lugar, nombre]);
 
 
-        res.status(200).json({ mensaje: 'Evento agregado exitosamente' });
-    } catch (error) {
-        console.error('Error al agregar el evento:', error);
-        res.status(500).json({ error: 'Error al agregar el evento' });
+    res.status(200).json({ mensaje: 'Evento agregado exitosamente' });
+  } catch (error) {
+    console.error('Error al agregar el evento:', error);
+    res.status(500).json({ error: 'Error al agregar el evento' });
 
-    }
+  }
 };
 
 
+// PUT
 const editarEvento = async (req, res) => {
-    const codigo_evento = (req.params.codigo_evento).substring(1);
+  const codigo_evento = (req.params.codigo_evento).substring(1);
 
-    const atributosActualizados = req.body;
+  const atributosActualizados = req.body;
 
-    try {
-        const eventoExistente = await evento.findByPk(codigo_evento);
+  try {
+    const eventoExistente = await evento.findByPk(codigo_evento);
 
-        if (!eventoExistente) {
-            return res.status(404).json({ message: 'El evento a editar no existe' });
-        }
-        const eventoActualizado = Object.assign(eventoExistente, atributosActualizados);
-        
-        await eventoActualizado.save();
-
-        return res.status(200).json({ message: 'Evento actualizado exitosamente'});
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: 'Error al actualizar el evento' });
+    if (!eventoExistente) {
+      return res.status(404).json({ message: 'El evento a editar no existe' });
     }
+    const eventoActualizado = Object.assign(eventoExistente, atributosActualizados);
+
+    await eventoActualizado.save();
+
+    return res.status(200).json({ message: 'Evento actualizado exitosamente' });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Error al actualizar el evento' });
+  }
 };
 
 module.exports = {
-    agregarEvento: agregarEvento,
-    editarEvento: editarEvento
+  agregarEvento: agregarEvento,
+  editarEvento: editarEvento
 };
--- controllers/getUserDataController.js
+++ controllers/getUserDataController.js
@@ -1,37 +1,19 @@
-const { pool } = require('../api/db.js');
+const persona = require('../models/persona.js');
 
 const getUserData = async (req, res) => {
   const { id } = req.body;
+  console.log(id);
 
   try {
-    // Consulta a la base de datos para obtener el usuario con el nickname proporcionado
-    const query = `
-          SELECT * FROM persona
-          WHERE id = $1
-          LIMIT 1
-        `;
-    const { rows } = await pool.query(query, [id]);
 
-    // if (rows.length === 0) {
-    //   return res.status(404).json({ error: 'El nickname no está registrado' });
-    // }
+    const userData = await persona.findByPk(id);
 
-    const usuario = {
-      "id": rows[0].id,
-      "tipo_de_usuario": rows[0].tipo_de_usuario,
-      "nickname": rows[0].nickname,
-      "nombre": rows[0].nombre,
-      "apellido": rows[0].apellido,
-      "genero": rows[0].genero,
-      "foto": rows[0].foto,
-      "correo_electronico": rows[0].correo_electronico,
-      "fecha_nacimiento": rows[0].fecha_nacimiento,
-      "descripcion": rows[0].descripcion,
-      "ciudad": rows[0].ciudad
+    if (!userData) {
+      return res.status(404).json({ error: 'El usuario no esta registrado' });
     }
 
     // El inicio de sesión fue exitoso
-    res.status(200).json(usuario);
+    res.status(200).json(userData);
 
   } catch (error) {
 
