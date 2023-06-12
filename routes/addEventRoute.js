0e6cd2d140840149ea687fbbbf93014f661daa66
Juan Felipe Arango <juan.felipe.arango@correounivalle.edu.co>
2023-06-12 15:42:24 -0500

    Se agrega routerObtenerEventosC

--- routes/addEventRoute.js
+++ routes/addEventRoute.js
@@ -1,17 +1,21 @@
 const express = require('express');
-const routerAgregarEvento = express.Router();
-const {agregarEvento, editarEvento} = require('../controllers/addEventController.js');
-
-const routerEditarEvento = express.Router();
+const {agregarEvento, editarEvento, obtenerEventosC} = require('../controllers/addEventController.js');
 const verificarAutenticacion = require('../middleware/verificarAutenticacion.js');
 
+const routerAgregarEvento = express.Router();
+const routerEditarEvento = express.Router();
+const routerObtenerEventosC = express.Router();
+
+
 routerEditarEvento.put('/evento/editar/:codigo_evento', verificarAutenticacion, editarEvento);
-
-
 routerAgregarEvento.post('/evento/agregar', verificarAutenticacion, agregarEvento);
+routerObtenerEventosC.get('/evento/obtenerC', verificarAutenticacion, obtenerEventosC);
+
+
 
 module.exports = {
     routerAgregarEvento: routerAgregarEvento,
-    routerEditarEvento: routerEditarEvento
+    routerEditarEvento: routerEditarEvento,
+    routerObtenerEventosC: routerObtenerEventosC
 };
 
