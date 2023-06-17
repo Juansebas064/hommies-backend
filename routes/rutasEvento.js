684c8211d7674ff8034a95a48b8abd300455c7b0
Juan Felipe Arango <juan.felipe.arango@correounivalle.edu.co>
2023-06-17 18:15:34 -0500

    Se crea routerAnularInscripcion

--- routes/rutasEvento.js
+++ routes/rutasEvento.js
@@ -1,21 +1,24 @@
 const express = require('express');
-const { agregarEvento, editarEvento, obtenerEventosC } = require('../controllers/controladorEvento.js');
+const { agregarEvento, editarEvento, obtenerEventosC, anularInscipcionEvento } = require('../controllers/controladorEvento.js');
 const verificarAutenticacion = require('../middleware/verificarAutenticacion.js');
 
 const routerAgregarEvento = express.Router();
 const routerEditarEvento = express.Router();
 const routerObtenerEventosC = express.Router();
+const routerAnularInscripcionEvento =express.Router();
 
 
 routerEditarEvento.put('/evento/editar/:codigo_evento', verificarAutenticacion, editarEvento);
 routerAgregarEvento.post('/evento/agregar', verificarAutenticacion, agregarEvento);
 routerObtenerEventosC.get('/evento/obtenerC', verificarAutenticacion, obtenerEventosC);
+routerAnularInscripcionEvento.delete('/evento/anular-inscripcion', verificarAutenticacion, anularInscipcionEvento);
 
 
 
 module.exports = {
   routerAgregarEvento: routerAgregarEvento,
   routerEditarEvento: routerEditarEvento,
-  routerObtenerEventosC: routerObtenerEventosC
+  routerObtenerEventosC: routerObtenerEventosC,
+  routerAnularInscripcionEvento: routerAnularInscripcionEvento
 };
 
