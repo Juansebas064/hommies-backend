05e84bd5b7cf20795addf68a0819da68203d4501
Juan Felipe Arango <juan.felipe.arango@correounivalle.edu.co>
2023-06-18 16:33:32 -0500

    Ahora se imprime error en interesesUsuario

--- controllers/controladorIntereses.js
+++ controllers/controladorIntereses.js
@@ -26,6 +26,7 @@ const { pool } = require('../api/db.js');
 
         res.status(200).json(intereses);
     } catch (error) {
+        console.log(error);
         res.status(500).json({error: 'Error en el servidor'});
     }
   }
