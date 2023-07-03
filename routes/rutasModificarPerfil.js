dcc049f9afb9dedb2b04793fa774f9958cb5c825
ClusterMax <carlosguerrero0705@gmail.com>
2023-07-03 14:21:11 -0500

    Modificar foto perfil solucion 2

--- routes/rutasModificarPerfil.js
+++ routes/rutasModificarPerfil.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const multer = require('multer')
 const routerModificar = express.Router();
 const modificarPerfil = require('../controllers/controladorModuloModificarPerfil.js');
 const verificarAutenticacion = require('../middleware/verificarAutenticacion.js');
 const { uploadAvatars } = require('../api/multer.js');
 
 routerModificar.put('/perfil/modificar', (req, res, next) => {
-  if (!req.file) {
-    return next();
-  }
-  uploadAvatars.single('foto')(req, res, next);
+  uploadAvatars.single('foto')(req, res, (error) => {
+    if (error instanceof multer.MulterError) {
+      // Ocurrió un error con Multer al subir el archivo
+      return next(error);
+    } else if (error) {
+      // Ocurrió un error general
+      return next(error);
+    }
+
+   
+    next();
+  });
 }, verificarAutenticacion, modificarPerfil);
 
 module.exports = routerModificar;
