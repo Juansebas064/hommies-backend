4a9df16731f1a2fac25c3a5149cb4d04c1dc652c
ClusterMax <carlosguerrero0705@gmail.com>
2023-07-03 17:10:03 -0500

    Modificar foto con campo vacio

--- routes/rutasModificarPerfil.js
+++ routes/rutasModificarPerfil.js
@@ -1,23 +1,13 @@
 const express = require('express');
-const multer = require('multer')
+
 const routerModificar = express.Router();
+
 const modificarPerfil = require('../controllers/controladorModuloModificarPerfil.js');
+
+
 const verificarAutenticacion = require('../middleware/verificarAutenticacion.js');
 const { uploadAvatars } = require('../api/multer.js');
 
-routerModificar.put('/perfil/modificar', (req, res, next) => {
-  uploadAvatars.single('foto')(req, res, (error) => {
-    if (error instanceof multer.MulterError) {
-      // Ocurrió un error con Multer al subir el archivo
-      return next(error);
-    } else if (error) {
-      // Ocurrió un error general
-      return next(error);
-    }
-
-   
-    next();
-  });
-}, verificarAutenticacion, modificarPerfil);
+routerModificar.put('/perfil/modificar', uploadAvatars.single('foto'),verificarAutenticacion, modificarPerfil);
 
 module.exports = routerModificar;
