8ec51732ec3e48b2a6dccf9942a4eb82f917d8ac
ClusterMax <carlosguerrero0705@gmail.com>
2023-07-03 13:14:31 -0500

    Modificar imagen vacio

--- routes/rutasModificarPerfil.js
+++ routes/rutasModificarPerfil.js
@@ -1,11 +1,14 @@
 const express = require('express');
-
 const routerModificar = express.Router();
 const modificarPerfil = require('../controllers/controladorModuloModificarPerfil.js');
 const verificarAutenticacion = require('../middleware/verificarAutenticacion.js');
 const { uploadAvatars } = require('../api/multer.js');
 
-routerModificar.put('/perfil/modificar', uploadAvatars.single('foto'),verificarAutenticacion, modificarPerfil);
-
+routerModificar.put('/perfil/modificar', (req, res, next) => {
+  if (!req.file) {
+    return next();
+  }
+  uploadAvatars.single('foto')(req, res, next);
+}, verificarAutenticacion, modificarPerfil);
 
 module.exports = routerModificar;
